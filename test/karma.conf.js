e0a786333ad06f642d99b67d0ad7e113fdb84300
Artur <rabenda27178@student.pwsztar.edu.pl>
2017-11-14 11:07:39 +0100

    add scaffolding

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config)
 
         // list of files / patterns to load in the browser
         files: ['app/bower_components/angular/angular.js', 'app/bower_components/angular-mocks/angular-mocks.js', 'app/*.js', 'app/!(bower_components)/**/*.js',
-                'test/unit/**/*.spec.js'],
+            'test/unit/**/*.spec.js'],
 
         // list of files / patterns to exclude
         exclude: [],
