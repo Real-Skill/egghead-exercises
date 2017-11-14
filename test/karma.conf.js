44fab8043fea3471eff40791affe612154618217
Artur <rabenda27178@student.pwsztar.edu.pl>
2017-11-14 09:47:28 +0100

    add scaffolding and test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,8 +1,3 @@
-// Karma configuration
-// http://karma-runner.github.io/0.12/config/configuration-file.html
-// Generated on 2014-09-07 using
-// generator-karma 0.8.3
-
 module.exports = function (config)
 {
     'use strict';
@@ -20,34 +15,25 @@ module.exports = function (config)
         },
 
         // list of files / patterns to load in the browser
-        files: ['../app/bower_components/angular/angular.js',
-                '../app/bower_components/angular-mocks/angular-mocks.js',
-                '../app/car.js',
-                'unit/**/*spec.js'],
+        files: ['../app/bower_components/angular/angular.js', '../app/bower_components/angular-mocks/angular-mocks.js', '../app/car.js', 'unit/**/*spec.js'],
 
         // list of files / patterns to exclude
         exclude: [],
 
-
-        reporters: ['spec', 'junit', 'coverage'],
+        reporters: ['spec', 'coverage', 'junit'],
 
         preprocessors: {
-            'app/modules/common/patchModel.service.js': 'coverage'
+            'app/*.js': 'coverage', 'app/!(bower_components)/**/*.js': 'coverage'
         },
 
         coverageReporter: {
-            dir: 'target/', reporters: [{
-                type: 'html'
-            }, {
-                type: 'cobertura', file: 'coverage.xml'
-            }]
-
+            dir: 'target/', type: 'cobertura', file: 'coverage.xml'
         },
+
 
         junitReporter: {
             outputFile: 'target/test-results.xml'
         },
-
 
         // web server port
         port: 8080,
@@ -67,7 +53,7 @@ module.exports = function (config)
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
-        singleRun: false,
+        singleRun: true,
 
         colors: true,
 
